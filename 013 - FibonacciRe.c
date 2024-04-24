2d79a15cf18a5e55a057321ac1ed0fd2a63b7392
Nany <165217126+KaylaneLimaKL@users.noreply.github.com>
2024-04-23 23:14:38 -0300

    Update 013 - FibonacciRe.c

--- 013 - FibonacciRe.c
+++ 013 - FibonacciRe.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
 
-int fibonacci(int tam) {
-    if (tam == 1) {
-        return 1; 
-    }
-    if (tam == 2) {
-        return 1; 
-    } else {
-        return fibonacci(tam - 1) + fibonacci(tam - 2);
-    }
+int fibonacci(int n){
+  if (n == 1){
+    return 1;
+  }
+  else if(n == 2){
+    return 1;
+  }
+  else{
+    return fibonacci(n-1) + fibonacci(n-2);
+  }
 }
 
-int main(void) {
-    int tam;
+int main (void){
+  int n;
 
-    printf(":: ");
-    scanf("%d", &tam);
+  printf("tamanho: ");
+  scanf ("%d", &n);
 
+  for (int i = 1; i<=n; i++){
+  printf("%d", fibonacci(i));
+  }
 
-      for (int i = 1; i <= tam; i++) {
-          printf("%d ", fibonacci(i));
-      }
-      printf("\n");
-
-    return 0;
+  
+  return 0;
 }
